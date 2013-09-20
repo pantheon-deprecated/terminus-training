cc618c1115cafceea08d808db887723b34aa3c60
Brian Wood <bwood@berkeley.edu>
2013-09-19 19:52:54 -0700

    basic cas user stuff

--- training-add-cas-user.php
+++ training-add-cas-user.php
@@ -2,17 +2,45 @@
 /*
  * Global variables
  */
+$verbose = true;
+$drush_dir = $_SERVER['HOME'] . '/.drush';
+$tmp_dir = $_SERVER['HOME'] . '/tmp';
 $drush = exec('which drush');
 $ldap_server = 'nds-test.berkeley.edu';
+
+//always add these admins
+$default_admins = array(
+  '213108', //bwood
+  '1043991', // instructor helen nishikai
+  '18493', // Kathleen Valerio
+  '212372', // test-212372
+  '212373', // test-212373
+  '212374', // test-212374
+);
+/*
+ * Functions
+ */
+function yesno($question) {
+  echo $question . " (y/n): ";
+  $handle = fopen ("php://stdin","r");
+  $line = fgets($handle);
+  if (strtolower(substr($line, 0, 1)) != 'y') {
+    echo "Aborting.\n";
+    exit(0);
+  }
+  echo "\n";
+  echo "Continuing...\n";
+}
 
 $usage = <<<EOT
 
 USAGE:
 
 php __FILE__ \
-  --emails_file \
-  --site_list \
+  --emails_file=/home/bwood/tmp/emails.txt \
+  --site_list=train-editor \
 
+(site list will be expanded to ~/.drush/train-editor.aliases.drushrc.php)
 EOT;
 
 if (!is_executable($drush)) {
@@ -21,6 +49,10 @@ if (!is_executable($drush)) {
   exit(1);
 }
 
+if (!is_writable($tmp_dir)) {
+  print "Need writable directory at $tmp_dir\n";
+  exit(1);
+}
 
 $longopts  = array(
   "emails_file:",     // Required value
@@ -30,7 +62,28 @@ $longopts  = array(
 $shortopts = "";
 $options = getopt($shortopts, $longopts);
 
+
+if (empty($options['emails_file']) || empty($options['site_list'])) {
+  print "Missing required options:\n";
+  print $usage . "\n";
+  exit(1);
+}
+
+
+$site_list_file = "$drush_dir/" . $options['site_list'] . ".aliases.drushrc.php";
+if (!file_exists($site_list_file)) {
+  print "File missing: $site_list_file";
+  exit(1);
+}
+
+if (!file_exists($options['emails_file'])) {
+  print "File missing: " . $options['emails_file'] . "\n";
+  exit(1);
+}
+
 $emails = explode("\n", file_get_contents($options['emails_file']));
+
+yesno("Have you updated your aliases with the training sites (drush paliases)?");
 
 /*
  * look up all with one query, problem is you can't tell which ones weren't
@@ -73,26 +126,57 @@ $dn = "ou=people,dc=berkeley,dc=edu";
 $attribs = array("uid");
 
 
+
+
 // lookup each email individually so we can tell which ones aren't found
 $uids = array();
+$missing = false;
 foreach ($emails as $email) {
-  $filter = "(mail=$email)";
-  $sr=ldap_search($ds, $dn, $filter, $attribs);
-  $r = ldap_get_entries($ds, $sr);
   //skip blank lines in file
   if (empty($email)) continue;
 
+  $filter = "(mail=$email)";
+  $sr=ldap_search($ds, $dn, $filter, $attribs);
+  $r = ldap_get_entries($ds, $sr);
 
   if (intval($r['count']) > 0) {
     $uids[] = $r[0]['uid'][0];
   }
   else {
+    $missing = true;
+    $h = fopen("$tmp_dir/emails_missing_uids.txt", "w");
+    fwrite($h, $email . "\n");
     print "$email: No UID\n";
   }
 }
 
-foreach ($uids as $uid) {
-  $cmd = "$drush " . $options['site_list'] . " cas-user-create $uid";
-
+if ($missing) {
+  print "Couldn't find some uids. Problem emails saved to $tmp_dir/emails_missing_uids.txt\n";
+  fclose($h);
 }
+else {
+  print "Found uids for all emails. Yea!\n";
+}
+
+
+
+yesno("Shall we add the uids we found (plus default_admins) to the sites?");
+
+$uids = array_merge($default_admins, $uids);
+
+$out = array();
+foreach ($uids as $uid) {
+  $cmd = "$drush -y @" . $options['site_list'] . " cas-user-create $uid";
+  if ($verbose) print "$cmd\n";
+  exec($cmd, $out);
+}
+
+$out_file = "$tmp_dir/" . __FILE__ ."_out.txt";
+$h = fopen($out_file, "w");
+fwrite($h, implode("\n". $out_file));
+fclose($h);
+print "See $out_file\n";
+
+
+
 
